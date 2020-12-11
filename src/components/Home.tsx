1995db3fd49eb35eca7ac624a807c3ee562e3be3
gaylonalfano <gaylon.alfano@gmail.com>
2020-12-11 15:46:32 +0800

    Completed NavBar and Home components. Would like to learn how to do a transparent navbar one day.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,18 @@
+import image from "../joe-woods-4Zaq5xY5M_c-unsplash.jpg";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Textured Brick Wall"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-gray-600 font-bold cursive leading-none lg:leading-snug home-name">
+          你好！I'm Gaylon.
+        </h1>
+      </section>
+    </main>
+  );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,51 @@
+import { NavLink } from "react-router-dom";
+/* import { SocialIcon } from "react-social-icons"; */
+
 export default function NavBar() {
-  return <h1>NavBar Page</h1>;
+  return (
+    <header className="bg-gray-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            className="inline-flex items-center py-6 px-3 mr-4 rounded-md text-gray-300 hover:text-white text-4xl font-bold cursive tracking-widest"
+            activeClassName="text-white"
+          >
+            Gaylon
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded-md text-gray-300 hover:bg-gray-700 hover:text-white"
+            activeClassName="bg-gray-900 text-white"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded-md text-gray-300 hover:bg-gray-700 hover:text-white"
+            activeClassName="bg-gray-900 text-white"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded-md text-gray-300 hover:bg-gray-700 hover:text-white"
+            activeClassName="bg-gray-900 text-white"
+          >
+            About me!
+          </NavLink>
+        </nav>
+        {/* <div className="inline-flex py-3 px-3 my-6"> */}
+        {/*   <SocialIcon */}
+        {/*     url="https://twitter.com/gaylonalfano" */}
+        {/*     className="mr-4" */}
+        {/*     target="_blank" */}
+        {/*     fgColor="#fff" */}
+        {/*     style={{ height: 35, width: 35 }} */}
+        {/*   /> */}
+        {/* </div> */}
+      </div>
+    </header>
+  );
 }
